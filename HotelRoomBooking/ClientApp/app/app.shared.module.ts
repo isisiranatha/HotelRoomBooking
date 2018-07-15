b95d8b190b018c6f3a09401978a66a98f38d7736
Indika Sisiranatha <indikas72@gmail.com>
2018-07-15 11:47:36 +1000

    Booking Form

--- HotelRoomBooking/ClientApp/app/app.shared.module.ts
+++ HotelRoomBooking/ClientApp/app/app.shared.module.ts
@@ -9,6 +9,9 @@ import { NavMenuComponent } from './components/navmenu/navmenu.component';
 import { HomeComponent } from './components/home/home.component';
 import { FetchDataComponent } from './components/fetchdata/fetchdata.component';
 import { CounterComponent } from './components/counter/counter.component';
+import { BookingComponent } from './components/booking/booking.component';
+
+
 
 @NgModule({
     declarations: [
@@ -16,7 +19,8 @@ import { CounterComponent } from './components/counter/counter.component';
         NavMenuComponent,
         CounterComponent,
         FetchDataComponent,
-        HomeComponent
+        BookingComponent,
+        HomeComponent        
     ],
     imports: [
         CommonModule,
@@ -25,6 +29,7 @@ import { CounterComponent } from './components/counter/counter.component';
         RouterModule.forRoot([
             { path: '', redirectTo: 'home', pathMatch: 'full' },
             { path: 'home', component: HomeComponent },
+            { path: 'booking', component: BookingComponent },
             { path: 'counter', component: CounterComponent },
             { path: 'fetch-data', component: FetchDataComponent },
             { path: '**', redirectTo: 'home' }
---
+++ HotelRoomBooking/ClientApp/app/components/booking/booking.component.ts
@@ -0,0 +1,13 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+    selector: 'booking',
+    templateUrl: './booking.component.html'
+})
+export class BookingComponent {
+    public currentCount = 0;
+
+    public incrementCounter() {
+        this.currentCount++;
+    }
+}
